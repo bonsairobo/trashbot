94c4b9d7228655a632c434778800ec0069190102
Duncan Fairbanks <fduncan@umich.edu>
2017-03-12 20:27:57 -0400

    refactor and add color stream

--- kinect/main.cpp
+++ kinect/main.cpp
@@ -16,22 +16,24 @@ void analyzeFrame(const VideoFrameRef& frame) {
     case PIXEL_FORMAT_DEPTH_1_MM:
     case PIXEL_FORMAT_DEPTH_100_UM:
         pDepth = (DepthPixel*)frame.getData();
-        printf("[%08llu] %8d\n", (long long)frame.getTimestamp(),
+        printf("[%08llu] %8d\n",
+            (long long)frame.getTimestamp(),
             pDepth[middleIndex]);
         break;
     case PIXEL_FORMAT_RGB888:
         pColor = (RGB888Pixel*)frame.getData();
-        printf("[%08llu] 0x%02x%02x%02x\n", (long long)frame.getTimestamp(),
+        printf("[%08llu] 0x%02x%02x%02x\n",
+            (long long)frame.getTimestamp(),
             pColor[middleIndex].r&0xff,
             pColor[middleIndex].g&0xff,
             pColor[middleIndex].b&0xff);
         break;
     default:
-        printf("Unknown format\n");
+        cout << "Unknown format" << endl;
     }
 }
 
-class PrintCallback : public VideoStream::NewFrameListener {
+class FrameCallback : public VideoStream::NewFrameListener {
 public:
     void onNewFrame(VideoStream& stream) {
         stream.readFrame(&m_frame);
@@ -42,8 +44,8 @@ private:
 };
 
 class OpenNIDeviceListener : public OpenNI::DeviceConnectedListener,
-                                    public OpenNI::DeviceDisconnectedListener,
-                                    public OpenNI::DeviceStateChangedListener
+                             public OpenNI::DeviceDisconnectedListener,
+                             public OpenNI::DeviceStateChangedListener
 {
 public:
     virtual void onDeviceStateChanged(
@@ -62,8 +64,32 @@ public:
     }
 };
 
-static void Sleep(int millisecs) {
+static void sleep(int millisecs) {
     usleep(millisecs * 1000);
+}
+
+static int try_start_video_stream(
+    VideoStream& stream,
+    Device& device,
+    SensorType type,
+    const string& type_str)
+{
+    Status rc;
+    if (device.getSensorInfo(type) != NULL) {
+        rc = stream.create(device, type);
+        if (rc != STATUS_OK) {
+            cout << "Couldn't create " << type_str <<  " stream" << endl
+                 << OpenNI::getExtendedError() << endl;
+            return 1;
+        }
+    }
+    rc = stream.start();
+    if (rc != STATUS_OK) {
+        cout << "Couldn't start the " << type_str << " stream" << endl
+             << OpenNI::getExtendedError() << endl;
+        return 1;
+    }
+    return 0;
 }
 
 int main() {
@@ -74,19 +100,19 @@ int main() {
         return 1;
     }
 
+    // Register callbacks for device events.
     OpenNIDeviceListener devicePrinter;
-
     OpenNI::addDeviceConnectedListener(&devicePrinter);
     OpenNI::addDeviceDisconnectedListener(&devicePrinter);
     OpenNI::addDeviceStateChangedListener(&devicePrinter);
 
+    // Enumerate devices and choose the first one.
     openni::Array<openni::DeviceInfo> deviceList;
     openni::OpenNI::enumerateDevices(&deviceList);
     for (int i = 0; i < deviceList.getSize(); ++i) {
         cout << "Device " << deviceList[i].getUri()
              << " already connected" << endl;
     }
-
     Device device;
     rc = device.open(ANY_DEVICE);
     if (rc != STATUS_OK) {
@@ -95,34 +121,23 @@ int main() {
         return 2;
     }
 
+    // Register callbacks for new frame events.
+    FrameCallback frame_cb;
     VideoStream depth;
+    if (try_start_video_stream(depth, device, SENSOR_DEPTH, "depth") != 0)
+        return 1;
+    depth.addNewFrameListener(&frame_cb);
+    VideoStream color;
+    if (try_start_video_stream(color, device, SENSOR_COLOR, "color") != 0)
+        return 1;
+    color.addNewFrameListener(&frame_cb);
 
-    if (device.getSensorInfo(SENSOR_DEPTH) != NULL) {
-        rc = depth.create(device, SENSOR_DEPTH);
-        if (rc != STATUS_OK)
-        {
-            cout << "Couldn't create depth stream" << endl
-                 << OpenNI::getExtendedError() << endl;
-        }
-    }
-    rc = depth.start();
-    if (rc != STATUS_OK) {
-        cout << "Couldn't start the depth stream" << endl
-             << OpenNI::getExtendedError() << endl;
-    }
-
-    PrintCallback depthPrinter;
-
-    // Register to new frame
-    depth.addNewFrameListener(&depthPrinter);
-
-    // Wait while we're getting frames through the printer
+    // Wait for new frames.
     while (true) {
-        Sleep(100);
+        sleep(100);
     }
 
-    depth.removeNewFrameListener(&depthPrinter);
-
+    depth.removeNewFrameListener(&frame_cb);
     depth.stop();
     depth.destroy();
     device.close();

b6065cbb2d43de12204548fa7ac9be8251ffb8a8
Duncan Fairbanks <fduncan@umich.edu>
2017-03-15 13:20:51 -0400

    forgot a step to get the driver linking properly

--- kinect/install_deps.sh
+++ kinect/install_deps.sh
@@ -23,6 +23,7 @@ cd libfreenect
 mkdir build
 cd build
 cmake -DBUILD_OPENNI2_DRIVER=ON -DCMAKE_BUILD_TYPE=RELEASE ..
+cp lib/OpenNI2-FreenectDriver/libFreenectDriver.so ../../OpenNI2/Bin/x64-Release/OpenNI2/Drivers/
 make
 cd $root_dir
 git clone https://github.com/occipital/OpenNI2.git

a99b27780ca6e2ad2f76a0564858e926ac7a9546
Duncan Fairbanks <fduncan@umich.edu>
2017-04-15 16:16:12 -0400

    simple PWM and bug fixes for kinect

--- arduino/sketch_mar21a.ino
+++ arduino/sketch_mar21a.ino
@@ -1,8 +1,8 @@
-int pinnum = 13;
+int pinnum = 13; // LED
 int enrpwm = 2;
 int enlpwm = 9;
 
-//m2 goes in reverse
+// m2 goes in reverse
 int m2in1l = 3;
 int m2in2l = 4;
 int m1in1r = 11;
@@ -20,83 +20,50 @@ void setup() {
 }
 
 void loop() {
-  
-      while (Serial.available() > 0) {
+    int ticks_high = 1;
+    int ticks_low = 1;
+    while (Serial.available() > 0) {
         char inChar = Serial.read();
         Serial.print(inChar);
-        if(inChar == 'w')
-        {
-        digitalWrite(pinnum, HIGH);
-      
-        digitalWrite(m1in1r, HIGH);
-        digitalWrite(m1in2r, LOW);
+        if (inChar == 'w') {
+            digitalWrite(pinnum, HIGH);
 
-        digitalWrite(m2in1l, HIGH);
-        digitalWrite(m2in2l, LOW);
-        digitalWrite(enrpwm, HIGH);
-        digitalWrite(enlpwm, HIGH);
-        delay(50);
-        }
-        if(inChar == 's')
-        {
-        digitalWrite(pinnum, LOW);
-   
-        digitalWrite(m1in1r, LOW);
-        digitalWrite(m1in2r, HIGH);
-       
-        digitalWrite(m2in1l, LOW);
-        digitalWrite(m2in2l, HIGH);
-        digitalWrite(enrpwm, HIGH);
-        digitalWrite(enlpwm, HIGH);
-        delay(50);
-        }
-        if(inChar == 'a')
-        {
-        digitalWrite(pinnum, HIGH);
-    
-        digitalWrite(m1in1r, HIGH);
-        digitalWrite(m1in2r, LOW);
-     
-        digitalWrite(m2in1l, LOW);
-        digitalWrite(m2in2l, HIGH);
-        digitalWrite(enlpwm, HIGH);
-        digitalWrite(enrpwm, HIGH);
-        delay(50);
-        }
-        if(inChar == 'd')
-        {
-        digitalWrite(pinnum, LOW);
-  
-        digitalWrite(m1in1r, LOW);
-        digitalWrite(m1in2r, HIGH);
- 
-        digitalWrite(m2in1l, HIGH);
-        digitalWrite(m2in2l, LOW);
-        digitalWrite(enrpwm, HIGH);
-        digitalWrite(enlpwm, HIGH);
-        delay(50);
-        }
-        
+            digitalWrite(m1in1r, HIGH);
+            digitalWrite(m1in2r, LOW);
+            digitalWrite(m2in1l, HIGH);
+            digitalWrite(m2in2l, LOW);
+        } else if (inChar == 's') {
+            digitalWrite(pinnum, LOW);
 
-      }
-        digitalWrite(pinnum, LOW);
-        digitalWrite(enrpwm, LOW);
-        digitalWrite(enlpwm, LOW);
-    
-    
-    // digitalWrite(enrpwm, HIGH);
-    // digitalWrite(m1in1r, HIGH);
-    // digitalWrite(m1in2r, LOW);
-    // digitalWrite(enlpwm, LOW);
-    // digitalWrite(m2in1l, HIGH);
-    // digitalWrite(m2in2l, LOW);
-    // delay(1000);
-    // digitalWrite(enrpwm, LOW);
-    // digitalWrite(m1in1r, HIGH);
-    // digitalWrite(m1in2r, LOW);
-    // digitalWrite(enlpwm, HIGH);
-    // digitalWrite(m2in1l, HIGH);
-    // digitalWrite(m2in2l, LOW);
-    // delay(1000);
+            digitalWrite(m1in1r, LOW);
+            digitalWrite(m1in2r, HIGH);
+            digitalWrite(m2in1l, LOW);
+            digitalWrite(m2in2l, HIGH);
+        } else if (inChar == 'a') {
+            digitalWrite(pinnum, HIGH);
+
+            digitalWrite(m1in1r, HIGH);
+            digitalWrite(m1in2r, LOW);
+            digitalWrite(m2in1l, LOW);
+            digitalWrite(m2in2l, HIGH);
+        } else if (inChar == 'd') {
+            digitalWrite(pinnum, LOW);
+
+            digitalWrite(m1in1r, LOW);
+            digitalWrite(m1in2r, HIGH);
+            digitalWrite(m2in1l, HIGH);
+            digitalWrite(m2in2l, LOW);
+        }
+
+        unsigned long start_ms = millis();
+        while (millis() - start_ms < 50) {
+            digitalWrite(enrpwm, HIGH);
+            digitalWrite(enlpwm, HIGH);
+            digitalWrite(enrpwm, LOW);
+            digitalWrite(enlpwm, LOW);
+        }
+    }
+    digitalWrite(pinnum, LOW);
+    digitalWrite(enrpwm, LOW);
+    digitalWrite(enlpwm, LOW);
 }
-
